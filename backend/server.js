658cd611a4a576de33c96aefc51e20ca142c3e17
delaquash <olaidemmauel1@gmail.com>
2021-04-28 11:39:56 +0100

    fixed mongoDB atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express =require('express') ;
+
+const dotenv =require('dotenv'); 
+
+const connectDB = require('./config/db');
+
 
 
 dotenv.config();
-
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
