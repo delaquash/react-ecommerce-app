0649ee8ae324de7a5b72e0e10753fe07e8ed97db
delaquash <olaidemmauel1@gmail.com>
2021-05-10 13:07:41 +0100

    react set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
+
 
 function App() {
   return (
     <>
-      <h1>Welcome to Delashop</h1>
+      <Header />
+        <main>
+          <h1>Welcome to Delashop</h1>
+        </main>
+      <Footer />
     </>
   );
 }
