736780dff5d70f3ace831593d8416fd65e53d46b
delaquash <olaidemmauel1@gmail.com>
2021-05-17 13:37:42 +0100

    add productConstant for state management

--- frontend/src/reducers/ProductReducers.js
+++ frontend/src/reducers/ProductReducers.js
@@ -1,10 +1,12 @@
+import { PRODUCT_LIST_FAILURE, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constant/productConstant';
+
 export const productListReducer =(state ={products: []}, action) => {
     switch(action.type){
-        case 'PRODUCT_LIST_REQUEST':
+        case PRODUCT_LIST_REQUEST:
             return{ loading: true, product: [] }
-        case 'PRODUCT_LIST_SUCCESS':
+        case PRODUCT_LIST_SUCCESS:
             return{ loading: false, product: action.payload }
-        case 'PRODUCT_LIST_FAILURE':
+        case PRODUCT_LIST_FAILURE:
             return{ loading: false, error: action.payload }
         default:
             return state
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,9 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer } from './reducers/ProductReducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+    productList: productListReducer,    
+})
 
 const initialState = {}
 
