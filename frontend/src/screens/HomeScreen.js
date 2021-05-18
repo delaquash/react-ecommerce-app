a5188dd1d9a335e025c817c0d1fc8bb10d6c4d27
delaquash <olaidemmauel1@gmail.com>
2021-05-18 05:53:12 +0100

    add redux state into homeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,29 +3,28 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import { listProducts } from '../actions/productAction';
-import products from '../product';
 
 
  const HomeScreen = () => {
      const dispatch = useDispatch()
 
-     
+     const productList = useSelector(state => state.productList)
+     const { loading, error, products } = productList
 
      useEffect(() => {
         dispatch(listProducts())
      }, [dispatch])
-
-     const products =[]
         return (
             <>
                 <h1>Latest Products</h1>
-                <Row>
+                {loading ? (<h3>Loading...</h3>) : error ? (<h3>{error}</h3>) :(<Row>
                     {products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                             <Product product={product} />
                         </Col>
                     ))}
-                </Row>   
+                </Row>)}
+                  
             </>
         )
     }
